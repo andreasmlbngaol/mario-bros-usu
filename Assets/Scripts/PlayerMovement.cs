9483b1591bd3c70344318e16bdd5c363b775cf9f
Andreas M Lbn Gaol <lgandre45@gmail.com>
2025-12-11 05:45:47 +0700

    Added "FlagPole" prefab and related components to the "1-1" scene. Implemented "FlagPole" script to handle flag animation and level completion sequence. Updated "PlayerMovement" script with OnEnable/OnDisable methods for improved state handling.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,40 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   private new Rigidbody2D rigidbody;
-   private Vector2 velocity;
-   private float inputAxis;
-   public float moveSpeed = 8f;
-   public float maxJumpHeight = 5f;
-
-   public float maxJumpTime = 1f;
-
-   public float jumpForce => (2f * maxJumpHeight) / (maxJumpTime/2f);
-
-   public float gravity => (-2f * maxJumpHeight) / Mathf.Pow((maxJumpTime/2f), 2);
+    private new Rigidbody2D rigidbody;
+    private Vector2 velocity;
+    private new Collider2D collider;
     
-   public bool grounded { get; private set;} 
+    private float inputAxis;
+    public float moveSpeed = 8f;
+    public float maxJumpHeight = 5f;
 
-   public bool jumping { get; private set; } 
-   public bool running =>Mathf.Abs(velocity.x) > 0.25 || Mathf.Abs(inputAxis) > 0.25f;
+    public float maxJumpTime = 1f;
 
-   public bool sliding => (inputAxis> 0f && velocity.x< 0f) || (inputAxis < 0f && velocity.x > 0f);
+    public float jumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2f);
 
-   private new Camera camera;
+    public float gravity => (-2f * maxJumpHeight) / Mathf.Pow((maxJumpTime / 2f), 2);
 
-   private void Awake()
+    public bool grounded { get; private set; }
+
+    public bool jumping { get; private set; }
+    public bool running => Mathf.Abs(velocity.x) > 0.25 || Mathf.Abs(inputAxis) > 0.25f;
+
+    public bool sliding => (inputAxis > 0f && velocity.x < 0f) || (inputAxis < 0f && velocity.x > 0f);
+
+    private new Camera camera;
+
+    private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        camera = Camera.main; 
+        camera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        rigidbody.isKinematic = false;
+        collider.enabled = true;
+        velocity = Vector2.zero;
+        jumping = false;
+    }
+    
+    private void OnDisable()
+    {
+        rigidbody.isKinematic = true;
+        collider.enabled = false;
+        velocity = Vector2.zero;
+        jumping = false;
     }
 
     private void Update()
-    //ini untuk input move nya 
+        //ini untuk input move nya 
     {
-        HorizontalMovement();  
+        HorizontalMovement();
         grounded = rigidbody.Raycast(Vector2.down);
 
         if (grounded)
@@ -43,52 +59,52 @@ public class PlayerMovement : MonoBehaviour
         }
 
         ApplyGravity();
-        
+
     }
 
     // ii transformasi manual itu 
     float MoveTowardsTransform(float current, float target, float delta)
-{
-    float transform = target - current;
+    {
+        float transform = target - current;
 
-    if (transform >  delta)
-        transform =  delta;
-    else if (transform < -delta)
-        transform = -delta;
+        if (transform > delta)
+            transform = delta;
+        else if (transform < -delta)
+            transform = -delta;
 
-    return current + transform;
-}
+        return current + transform;
+    }
 
 
-   private void HorizontalMovement()
-{
-    inputAxis = Input.GetAxis("Horizontal");
+    private void HorizontalMovement()
+    {
+        inputAxis = Input.GetAxis("Horizontal");
 
-    float targetVelocity = inputAxis * moveSpeed;
-    float delta = moveSpeed * Time.deltaTime;
+        float targetVelocity = inputAxis * moveSpeed;
+        float delta = moveSpeed * Time.deltaTime;
 
-    velocity.x = MoveTowardsTransform(
-        velocity.x,
-        targetVelocity,
-        delta
-    );
+        velocity.x = MoveTowardsTransform(
+            velocity.x,
+            targetVelocity,
+            delta
+        );
 
-    if(rigidbody.Raycast(Vector2.right * velocity.x))
+        if (rigidbody.Raycast(Vector2.right * velocity.x))
         {
-            velocity.x= 0f;
+            velocity.x = 0f;
         }
 
         if (velocity.x > 0f)
         {
             transform.eulerAngles = Vector3.zero;
         }
-        else if(velocity.x < 0f)
+        else if (velocity.x < 0f)
         {
-         transform.eulerAngles = new Vector3(0f, 180f,0f);   
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
         }
-}
+    }
 
-private void GroundedMovement()
+    private void GroundedMovement()
     {
         velocity.y = Mathf.Max(velocity.y, 0f);
         jumping = velocity.y > 0f;
@@ -100,30 +116,31 @@ private void GroundedMovement()
         }
     }
 
-private void ApplyGravity()
+    private void ApplyGravity()
     {
         bool falling = velocity.y < 0f || !Input.GetButton("Jump");
         float multiplier = falling ? 2f : 1f;
 
         velocity.y += gravity * multiplier * Time.deltaTime;
-        velocity.y= Mathf.Max(velocity.y, gravity/2f);
+        velocity.y = Mathf.Max(velocity.y, gravity / 2f);
     }
+
     private void FixedUpdate()
     {
         Vector2 position = rigidbody.position;
-        position+= velocity * Time.fixedDeltaTime;
+        position += velocity * Time.fixedDeltaTime;
         Vector2 leftEdge = camera.ScreenToWorldPoint(Vector2.zero);
         Vector2 rightEdge = camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
-        position.x = Mathf.Clamp(position.x, leftEdge.x + 0.5f, rightEdge.x-0.5f);
+        position.x = Mathf.Clamp(position.x, leftEdge.x + 0.5f, rightEdge.x - 0.5f);
 
 
-        rigidbody.MovePosition(position);   
-    } 
+        rigidbody.MovePosition(position);
+    }
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             if (transform.DotTest(collision.transform, Vector2.down))
             {
@@ -131,14 +148,13 @@ private void ApplyGravity()
                 jumping = true;
             }
         }
-        else if(collision.gameObject.layer != LayerMask.NameToLayer("PowerUp"))
+        else if (collision.gameObject.layer != LayerMask.NameToLayer("PowerUp"))
         {
-            if(transform.DotTest(collision.transform, Vector2.up))
+            if (transform.DotTest(collision.transform, Vector2.up))
             {
                 velocity.y = 0f;
             }
 
         }
     }
-
 }
